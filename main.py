0f4b97bf07496fe936c3d6bf16cc3b026dcca9d3
rhfcodeshaman <81051405+rhfcodeshaman@users.noreply.github.com>
2024-12-28 11:00:36 -0500

    24-12-28 - Faction Object Creation
    Working on the logic to load Faction objects from the setting YAML.

--- main.py
+++ main.py
@@ -20,17 +20,27 @@ class Campaign:
 
 class Faction:
 
-    def __init__(self, fac_name, fac_tier,
-                 fac_goal, fac_clock):
+    def __init__(self, name, tier, description,
+                 turf, npcs, notable_assets,
+                 quirks, allies, enemies,
+                 situation, clocks):
         """
         The faction class will take in information from
         the setting YAML.
         Args:
-            - Faction name
-            - Faction tier
-            - Faction goal(s)
-            - Goal clock(s)
+            - A lot lol
         """
+        self.name = name
+        self.tier = tier
+        self.description = description
+        self.turf = turf
+        self.npcs = npcs
+        self.noteable = notable_assets
+        self.quirks = quirks
+        self. allies = allies
+        self.enemies = enemies
+        self.situation = situation
+        self.clocks = clocks
 
 class Game:
 
@@ -40,6 +50,37 @@ class Game:
         Args:
 
         """
+
+    def load_faction_stats(self, faction_list, faction_entry):
+        """
+        Accepts an entry from the selected Factions YAML
+        and loads it into a dictionary.
+        Args:
+        - Faction_name: the YAML entry for the faction
+        """
+        with open(faction_list, 'r') as f:
+            factions = yaml.safe_load(f)
+            for faction in factions:
+                if faction_entry in faction:
+                    return faction[faction_entry]
+        return None
+
+    def create_faction_object(self, faction_list):
+        """
+        Takes the dictionary returned from load_faction_stats
+        and creates a Faction object for that Game instance.
+        Args:
+        - faction_stats: dict created by load_faction_stats
+        """
+        for factions in faction_list:
+            # load each faction entry as a dictionary
+            # parses it to a new Faction object called
+            # name = Faction(self, name = name
+
+    def create_all_factions(self, faction_list):
+        for factions in faction_list:
+            self.create_faction_object()
+
 
 # This function is used to start a new Campaign
 def new_campaign():
@@ -106,14 +147,14 @@ right_frame = Frame(root)
 right_frame.pack(side="right", fill="y")
 
 # Run Advancement Button
-# run_advancement_button = Button(right_frame, text="Run Advancement", command=run_advancement)
-# run_advancement_button.pack(pady=5)
+run_advancement_button = Button(right_frame, text="Run Advancement", command=run_advancement)
+run_advancement_button.pack(pady=5)
 
 # Create New Faction Button
 # create_new_faction_button = Button(right_frame, text="Create New Faction", command=create_new_faction)
 # create_new_faction_button.pack(pady=5)
-
-# Edit Faction Button
+#
+# # Edit Faction Button
 # edit_faction_button = Button(right_frame, text="Edit Faction", command=edit_faction)
 # edit_faction_button.pack(pady=5)
 
